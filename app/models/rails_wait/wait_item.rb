a96cb89bf3c847219df91498f1870e3363c8f84e
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-08 13:08:25 +0800

    support notice

--- app/models/rails_wait/wait_item.rb
+++ app/models/rails_wait/wait_item.rb
@@ -7,7 +7,27 @@ module RailsWait::WaitItem
 
     belongs_to :user
     belongs_to :wait_list, counter_cache: true
+
+    after_save_commit :to_notice, if: -> { saved_change_to_volunteer_id? && volunteer }
+    acts_as_notify(
+      :default,
+      only: [:position],
+      methods: [:state_i18n, :address]
+    )
   end
 
+  def address
+    "#{wait_list.address.content}"
+  end
+
+  def to_notice
+    to_notification(
+      receiver: user,
+      title: '您的快递已备好',
+      body: '请即刻出发前往领取',
+      link: url_helpers.my_wait_item_url(id),
+      verbose: true
+    )
+  end
 
 end
