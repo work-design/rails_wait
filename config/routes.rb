68ea91697a4ea78bdf31a2b2de98b254a381e648
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-22 22:14:37 +0800

    defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  scope :admin, module: 'wait/admin', as: :admin do
+  scope :admin, module: 'wait/admin', as: :admin, defaults: { namespace: 'admin', business: 'wait' } do
     resources :wait_taxons
   end
 
